458f186e4a09f097c5377ca5a7d1e80fed8127da
Barry-zong <1804917712@qq.com>
2025-11-02 16:55:30 -0500

    local server finish

--- Week1/server.js
+++ Week1/server.js
@@ -3,9 +3,35 @@ let fs = require('fs');
 
 let myServer = http.createServer(myRequestHandler) 
 
-myServer.listen(8080);
 
-function myRequestHandler(req) {
+
+myServer.listen(8080,() =>{console.log("Server is listening on port 8080, http://localhost:8080/");});
+
+function myRequestHandler(req,res) {
 // respond incoming requests
- console.log(req);
+    let path = req.url;
+ console.log( 'incoming request:   ',req.url);
+
+ console.log('Server running in this directory: ' + __dirname);
+ let filePath = __dirname + path ;
+ console.log('Requested file path: ' + filePath);
+
+   fs.readFile(filePath, function(err, data){
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+     console.log("got file data");
+     res.writeHead(200);
+     res.end(data);
+   });
+
+
+
+//  res.statusCode = 200;
+//  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+//  res.end('Hello, World11!\n');
+
+
 }
